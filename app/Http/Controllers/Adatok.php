f79ba59e60337af8c14b9f3b61289c46b959981d
Bandi <dandris123@gmail.com>
2022-11-06 19:28:24 +0100

    form mentés és form validáció

--- app/Http/Controllers/Adatok.php
+++ app/Http/Controllers/Adatok.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Adatok as AdatokModel;
 
 class Adatok extends Controller
 {
@@ -13,5 +14,18 @@ class Adatok extends Controller
 
     public function store(Request $request)
     {
+        $validated = $request->validate([
+            'vezeteknev' => 'required|max:255',
+            'keresztnev' => 'required|max:255',
+            'szuletesiev' => 'required|digits:4',
+        ]);
+
+        $object = new AdatokModel;
+        $object->vezeteknev = $request->vezeteknev;
+        $object->keresztnev = $request->keresztnev;
+        $object->szuletesiev = $request->szuletesiev;
+        $object->save();
+
+        return redirect('/');
     }
 }
